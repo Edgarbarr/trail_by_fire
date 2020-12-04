e5d62672b22dd20db3aee6d25c805b0484f63705
Edgar Barrientos <edgarbarrientos@Edgars-MBP.lan>
2020-12-04 12:27:29 -0800

    dotenv

--- cat.js
+++ cat.js
@@ -1,15 +1,11 @@
+require("dotenv").config();
 const catme = require("cat-me");
 const axios = require("axios");
+axios.get(process.env.URL).then((res) => {
+  let gifs = res.data.data;
 
-axios
-  .get(
-    "http://api.giphy.com/v1/gifs/search?api_key=TZMzIVU9PeU9S5uUXXBSKZrBfRv9woGI&q=kanye&limit=12"
-  )
-  .then((res) => {
-    let gifs = res.data.data;
-
-    gifs.map((gif) => {
-      console.log(gif.images.downsized.url);
-    });
+  gifs.map((gif) => {
+    console.log(gif.images.downsized.url);
   });
+});
 console.log(catme("grumpy"));
